fd771533477e1d1b65202a94f4007e4353df35df
Chase Stokes <chasehunterstokes@gmail.com>
2022-02-24 09:59:30 -0800

    good fail on second letter a test
    Co-authored-by: Kevin Williams <k9w@users.noreply.github.com>

--- ScrabbleScore.Tests/ModelTests/ScrabbleScoreTests.cs
+++ ScrabbleScore.Tests/ModelTests/ScrabbleScoreTests.cs
@@ -7,10 +7,33 @@ namespace ScrabbleScore.TestTools
   [TestClass]
   public class ScoreTests
   {
+    // [TestMethod]
+    // public void Method_Description_ExpectedOutput()
+    // {
+    //   Assert.AreEqual("output", Class.Method("input"));
+    // }
+    
     [TestMethod]
     public void CheckScore_AReturnsOnePoint_1()
     {
       Assert.AreEqual("1", Score.CheckScore("a"));
     }
+    
+    [TestMethod]
+    public void CheckScore_EReturnsOnePoint_1()
+    {
+      Assert.AreEqual("1", Score.CheckScore("e"));
+    }
+
   }
 }
+
+
+
+// A, E, I, O, U, L, N, R, S, T          1
+// D, G                                  2
+// B, C, M, P                            3
+// F, H, V, W, Y                         4
+// K                                     5
+// J, X                                  8
+// Q, Z                                 10

d3eb15b9d0a77c0653ec103dde8c7966c6b885a3
Chase Stokes <chasehunterstokes@gmail.com>
2022-02-24 15:12:52 -0800

    cleaned up commented out code
    Co-authored-by: Ryan Bass <probablynotryan@users.noreply.github.com>
    Co-authored-by: Kevin Williams <k9w@users.noreply.github.com>

--- ScrabbleScore/Models/ScrabbleScore.cs
+++ ScrabbleScore/Models/ScrabbleScore.cs
@@ -7,32 +7,14 @@ namespace ScrabbleScore
   {
     public static int CheckScore(string input)
     {
-
-      //Refactored Previous Code to use a loop
-
-      // int[] PointValues = {1, 2, 3, 4, 5, 8, 10};
-      string[] RegExpressionList = { "!", "[aeioulnrst]", "[dg]", "[bcmp]", "[fhvwy]", "[k]", "!", "!", "[jx]", "!", "[qz]"};
-      int rollingTotal = 0;
-      int i = 0;
+      string[] RegExpressionList = { "!", "[aieoulnrst]", "[dg]", "[bcmp]", "[fhvwy]", "[k]", "!", "!", "[jx]", "!", "[qz]"};
+      int rollingTotal = 0, i = 0;
       foreach(string element in RegExpressionList)
       {
-        // you can change {* i} with PointValues[i] and remove "!" from RegExpressionList to get the same result
         rollingTotal += Regex.Matches(input, element, RegexOptions.IgnoreCase).Count * i; 
         i++;
       }
       return rollingTotal;
-
-    // Previous Code
-
-      // int OnePointSquares = Regex.Matches(input, "[aeioulnrst]", RegexOptions.IgnoreCase).Count * 1;
-      // int TwoPointSquares = Regex.Matches(input, "[dg]", RegexOptions.IgnoreCase).Count * 2;
-      // int ThreePointSquares =  Regex.Matches(input, "[bcmp]", RegexOptions.IgnoreCase).Count * 3;
-      // int FourPointSquares =  Regex.Matches(input, "[fhvwy]", RegexOptions.IgnoreCase).Count * 4;
-      // int FivePointSquares =  Regex.Matches(input, "[k]", RegexOptions.IgnoreCase).Count * 5;
-      // int EightPointSquares =  Regex.Matches(input, "[jx]", RegexOptions.IgnoreCase).Count * 8;
-      // int TenPointSquares =  Regex.Matches(input, "[qz]", RegexOptions.IgnoreCase).Count * 10;
-      // int TotalPoints = OnePointSquares + TwoPointSquares + ThreePointSquares + FourPointSquares + FivePointSquares + EightPointSquares + TenPointSquares;
-      // return TotalPoints;
     }
   }
 }
